879c0107bcb04d630f3d5c18373a551242aa2344
ETEC HORACIO <email@email.com>
2022-10-21 22:24:39 -0300

    sintax

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,21 @@
-import { getByAltText, render } from '@testing-library/react-native'
+import { getByText, render } from '@testing-library/react-native'
 import Home from './pages/Home'
 
 
-test('sum', () => {
-    const { getByText } = render(<Home />)
+// test('sum', () => {
+//     const { getByText } = render(<Home />)
 
-    expect(getByText('List')).toBeTruthy()
+//     expect(getByText('List')).toBeTruthy()
+// })
+
+
+describe('', () => {
+    it('Should render home component', () => {
+        const { getByText } = render(<Home />);
+
+        expect(getByText('List')).toBeTruthy();
+    });
+    it();
+    it();
+    it();
 })
